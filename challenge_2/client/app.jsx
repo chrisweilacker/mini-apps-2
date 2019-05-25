384beb9b06a6a4fb55a57c4f2dd0a11c4920794c
Christopher Weilacker <chrisweilacker@gmail.com>
2019-05-25 15:27:45 +0800

    cryto chart [feature] graph displaying

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,52 +1,170 @@
-var Chart = require('chart.js')
+var Chart = require('chart.js');
+var moment = require('moment');
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Axios from 'axios';
+var chart = null;
+
 
 class App extends Component{
     constructor(props) {
         super(props)
         this.state = {
             time: '1D',
-            currency: '5D',
+            currency: 'USD',
             crypto: 'BTC'
         };
     }
     
+
+
+
+    
     renderChart() {
         var ctx = document.getElementById('myChart');
         let serverResponse=null;
+        let formatString='';
         if (this.state.time=='5Y') {
-            Axios.get('http://localhost:9001/cyrpto/daily')
+            formatString='MMMM YYYY';
+            serverResponse=Axios.get('http://localhost:9001/cyrpto/daily', {
+                params: {
+                    fsym: this.state.crypto,
+                    tsym: this.state.currency,
+                    aggregate: 30,
+                    limit: 61
+                }
+            });
         } else if (this.state.time=='1Y') {
-            Axios.get('http://localhost:9001/cyrpto/daily')
+            formatString='MMMM YYYY';
+            serverResponse=Axios.get('http://localhost:9001/cyrpto/daily', {
+                params: {
+                    fsym: this.state.crypto,
+                    tsym: this.state.currency,
+                    aggregate: 31,
+                    limit: 12
+                }
+            });
         } else if (this.state.time=='1M') {
-            Axios.get('http://localhost:9001/cyrpto/hourly')
+            formatString='MMMM DD YYYY';
+            serverResponse=Axios.get('http://localhost:9001/cyrpto/daily', {
+                params: {
+                    fsym: this.state.crypto,
+                    tsym: this.state.currency,
+                    aggregate: 1,
+                    limit: 31
+                }
+            });
         } else if (this.state.time=='5D') {
-            Axios.get('http://localhost:9001/cyrpto/hourly')
+            formatString='MMMM DD LT';
+            serverResponse=Axios.get('http://localhost:9001/cyrpto/hourly', {
+                params: {
+                    fsym: this.state.crypto,
+                    tsym: this.state.currency,
+                    aggregate: 2,
+                    limit: 60
+                }
+            });
         } else {
-            Axios.get('http://localhost:9001/cyrpto/hourly')
+            formatString='MMMM DD YYYY LT';
+            serverResponse=Axios.get('http://localhost:9001/cyrpto/minute', {
+                params: {
+                    fsym: this.state.crypto,
+                    tsym: this.state.currency,
+                    aggregate: 15,
+                    limit: 96
+                }
+            });
         }
         serverResponse.then((response)=> {
-            console.log(response.data);
-        })
-        //get default start data and create a chart
-        var myChart = new Chart(ctx, {
+            let sanitizedData = [];
+            for (let i =0; i<response.data.length; i++) {
+                let newDate = new Date(response.data[i].time*1000);
+                let formattedDate = moment(newDate).format(formatString);
+                sanitizedData.push({t: formattedDate, y:response.data[i].close})
+            }
+            console.log(sanitizedData);
 
-        });
+            let chartColors = {
+                red: 'rgb(255, 99, 132)',
+                orange: 'rgb(255, 159, 64)',
+                yellow: 'rgb(255, 205, 86)',
+                green: 'rgb(75, 192, 192)',
+                blue: 'rgb(54, 162, 235)',
+                purple: 'rgb(153, 102, 255)',
+                grey: 'rgb(201, 203, 207)'
+            };
+            
+            var color = Chart.helpers.color;
+            var cfg = {
+                type: 'bar',
+                data: {
+                    datasets: [{
+                        label: `${this.state.crypto} : ${this.state.currency} - ${this.state.time}`,
+                        backgroundColor: color(chartColors.red).alpha(0.5).rgbString(),
+                        borderColor: chartColors.red,
+                        data: sanitizedData,
+                        type: 'line',
+                        pointRadius: 0,
+                        fill: false,
+                        lineTension: 0,
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    scales: {
+                        xAxes: [{
+                            type: 'time',
+                            distribution: 'series',
+                            ticks: {
+                                source: 'data',
+                                autoSkip: true
+                            }
+                        }],
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: `Price (${this.state.currency})`
+                            }
+                        }]
+                    },
+                    tooltips: {
+                        intersect: false,
+                        mode: 'index',
+                        callbacks: {
+                            label: function(tooltipItem, myData) {
+                                var label = myData.datasets[tooltipItem.datasetIndex].label || '';
+                                if (label) {
+                                    label += ': ';
+                                }
+                                label += parseFloat(tooltipItem.value).toFixed(2);
+                                return label;
+                            }
+                        }
+                    }
+                }
+            };
+            if (chart!==null) {
+                chart.destroy();
+            }
+            chart = new Chart(ctx, cfg);
+
+            
+            
+
+	    });
     }
 
     render() {
-        //this.renderChart();
+        this.renderChart();
         return( 
-            <div className="row">
+            <div className="row" style={{'margin-bottom':'20px'}}>
                 <div className="col-6">
-                    <h1>CrytoCurrency Timeline Chart</h1>
+                    <h1>CrytoCurrency Chart</h1>
                 </div>
                 <div className="col-2">
                     <label for="crypto">CryptoCurrency: </label>  
                     <select class="form-control" onChange={(e)=>{this.setState({crypto: e.target.value})}} id="crypto">
-                        <option selected value="BTC">BitCoin (BTC)</option>
+                        <option value="BTC">BitCoin (BTC)</option>
                         <option value="LTC">Litecoin (LTC)</option>
                         <option value="ETH">Ethereum (ETH)</option>
                     </select>   
@@ -54,7 +172,7 @@ class App extends Component{
                 <div className="col-2">
                   <label for="currency">Fiat Currency: </label>  
                   <select class="form-control" onChange={(e)=>{this.setState({currency: e.target.value})}} id="currency">
-                    <option selected value="USD">US Dollar (USD)</option>
+                    <option value="USD">US Dollar (USD)</option>
                     <option value="CAD">Canadian Dollar (CAD)</option>
                     <option value="GBP">British Pound (GBP)</option>
                     <option value="EUR">Euro (EUR)</option>
@@ -64,7 +182,7 @@ class App extends Component{
                 <div className="col-2">
                   <label for="timespan">TimeSpan: </label>  
                   <select class="form-control" onChange={(e)=>{this.setState({time: e.target.value})}} id="timespan">
-                    <option selected value="1D">One Day</option>
+                    <option value="1D">One Day</option>
                     <option value="5D">Five Days</option>
                     <option value="1M">One Month</option>
                     <option value="1Y">One Year</option>
@@ -74,6 +192,14 @@ class App extends Component{
                 
             </div>);
     }
+}
+
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
 }
 
 ReactDOM.render(<App/>, document.getElementById('App'));
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,20 +1,49 @@
 var express = require('express');
 var app = express();
+var Axios = require('axios');
+var bodyParser = require('body-parser')
+var api = require('./config.js').apikey;
 var cache = {};
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/cyrpto/daily', (req,res)=>{
 //https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10
+Axios.get(`https://min-api.cryptocompare.com/data/histoday?fsym=${req.query.fsym}&tsym=${req.query.tsym}&aggregate=${req.query.aggregate}&limit=${req.query.limit}`, {
+    headers: {
+        'Apikey': api
+    }
+})
+.then((response)=>{
+    res.send(response.data.Data);
+});
 })
 
 app.get('/cyrpto/hourly', (req,res)=>{
 //https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=10
+Axios.get(`https://min-api.cryptocompare.com/data/histohour?fsym=${req.query.fsym}&tsym=${req.query.tsym}&aggregate=${req.query.aggregate}&limit=${req.query.limit}`, {
+    headers: {
+        'Apikey': api
+    }
+})
+.then((response)=>{
+    res.send(response.data.Data);
+});
 })
 
 app.get('/cyrpto/minute', (req,res)=>{
-//https://min-api.cryptocompare.com/data/histominute?fsym=BTC&tsym=GBP&limit=10
-})
+    console.log(req.originalUrl);
+    console.log(req.query.fsym);
+    Axios.get(`https://min-api.cryptocompare.com/data/histominute?fsym=${req.query.fsym}&tsym=${req.query.tsym}&aggregate=${req.query.aggregate}&limit=${req.query.limit}`, {
+        headers: {
+            'Apikey': api
+        }
+    })
+    .then((response)=>{
+        res.send(response.data.Data);
+    });
+});
 
 app.listen(9001, ()=>{
     console.log('listening on port 9001');
